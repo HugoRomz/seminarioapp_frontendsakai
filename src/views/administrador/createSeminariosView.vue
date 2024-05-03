76654f788958496714b3d3f957c1082bd9c8f936
HugoRomz <rosalesrafael1@hotmail.com>
2024-05-02 18:49:05 -0600

    CreateSeminario completo

--- src/views/administrador/createSeminariosView.vue
+++ src/views/administrador/createSeminariosView.vue
@@ -7,122 +7,102 @@ import Spinner from '../../components/Spinner.vue';
 const isAccepting = ref(false);
 const toast = inject('toast');
 
-const carreras = ref(null);
+const carreras = ref([]);
+const materias = ref([]);
+const cursosTable = ref(null);
 const filters = ref();
 const loading = ref(null);
-const dt = ref();
+const expandedRows = ref([]);
 
 const isEditMode = ref(false);
 
-const carreraEdit = ref({});
-const deleteCarreraModal = ref(false);
-const carreraModal = ref(false);
+const cursosEdit = ref({});
+const cursosModal = ref(false);
 const submitted = ref(false);
 
-const loadCarreras = async () => {
+const loadCursos = async () => {
     loading.value = true;
     try {
-        const response = await catalogoApi.findCarreras();
-        carreras.value = response.data;
+        const response = await catalogoApi.findCursos();
+        cursosTable.value = response.data;
     } catch (error) {
         console.error('Error al obtener los usuarios:', error);
     } finally {
         loading.value = false;
     }
 };
+const findCarreras = async () => {
+    try {
+        const response = await catalogoApi.findCarreras();
+        carreras.value = response.data;
+    } catch (error) {
+        console.error('Error al obtener las carreras:', error);
+    }
+};
+const findMaterias = async () => {
+    try {
+        const response = await catalogoApi.findMaterias();
+        materias.value = response.data;
+    } catch (error) {
+        console.error('Error al obtener las carreras:', error);
+    }
+};
 
-onMounted(loadCarreras);
+onMounted(loadCursos(), findCarreras(), findMaterias());
 
 const openNew = () => {
-    carreraEdit.value = {};
+    cursosEdit.value = {};
     submitted.value = false;
-    carreraModal.value = true;
+    cursosModal.value = true;
     isEditMode.value = false;
 };
 
 const hideDialog = () => {
-    carreraModal.value = false;
+    cursosModal.value = false;
     submitted.value = false;
 };
 
-const saveCarrera = async () => {
+const saveCurso = async () => {
     submitted.value = true;
     isAccepting.value = true;
-    if (carreraEdit.value.nombre_carrera.trim()) {
-        loading.value = true;
-        try {
-            let response;
-            if (isEditMode.value) {
-                response = await catalogoApi.updateCarrera(carreraEdit.value);
-                toast.open({
-                    message: response.data.msg,
-                    type: 'success'
-                });
-            } else {
-                response = await catalogoApi.createCarrera(carreraEdit.value);
-                toast.open({
-                    message: response.data.msg,
-                    type: 'success'
-                });
-            }
-            carreraModal.value = false;
-            carreraEdit.value = {};
-            isEditMode.value = false;
-            await loadCarreras();
-        } catch (error) {
-            toast.open({
-                message: error.response && error.response.data.msg ? error.response.data.msg : 'Error desconocido',
-                type: 'error'
-            });
-        } finally {
-            loading.value = false;
-            isAccepting.value = false;
-        }
-    } else {
-        isAccepting.value = false;
-        toast.open({
-            message: 'Por favor, completa todos los campos requeridos.',
-            type: 'warn'
-        });
-    }
-};
 
-const confirmDeleteMateria = (editCarrera) => {
-    carreraEdit.value = editCarrera;
-    deleteCarreraModal.value = true;
-};
-
-const editCarrera = (editCarrera) => {
-    const materiasData = { ...editCarrera };
-    carreraEdit.value = materiasData;
-    carreraModal.value = true;
-    isEditMode.value = true;
-};
-
-const deleteCarrera = async () => {
-    isAccepting.value = true;
+    loading.value = true;
 
     try {
-        const response = await catalogoApi.deleteCarrera(carreraEdit.value.carrera_id);
-        toast.open({
-            message: response.data.msg,
-            type: 'success'
-        });
-        deleteCarreraModal.value = false;
-        carreraEdit.value = {};
-        await loadCarreras(); // Recarga la lista de usuarios
+        let response;
+        if (isEditMode.value) {
+            response = await catalogoApi.updateCurso(cursosEdit.value);
+            toast.open({
+                message: response.data.msg,
+                type: 'success'
+            });
+        } else {
+            response = await catalogoApi.createCurso(cursosEdit.value);
+            toast.open({
+                message: response.data.msg,
+                type: 'success'
+            });
+        }
+        cursosModal.value = false;
+        cursosEdit.value = {};
+        isEditMode.value = false;
+        await loadCursos();
     } catch (error) {
         toast.open({
-            message: error.response && error.response.data.msg ? error.response.data.msg : 'Error desconocido al eliminar',
+            message: error.response && error.response.data.msg ? error.response.data.msg : 'Error desconocido',
             type: 'error'
         });
     } finally {
+        loading.value = false;
         isAccepting.value = false;
     }
 };
 
-const exportCSV = () => {
-    dt.value.exportCSV();
+const editCursos = (curso) => {
+    const cursoData = { ...curso };
+    cursosEdit.value = { ...cursoData, carrera: cursoData.carrera };
+    cursosModal.value = true;
+    isEditMode.value = true;
 };
 
 const initFilters = () => {
@@ -130,7 +110,6 @@ const initFilters = () => {
         global: { value: null, matchMode: FilterMatchMode.CONTAINS }
     };
 };
-
 initFilters();
 const clearFilter = () => {
     initFilters();
@@ -147,22 +126,17 @@ const clearFilter = () => {
                             <Button label="Nueva Carrera" icon="pi pi-plus" class="mr-2" severity="success" @click="openNew" />
                         </div>
                     </template>
-
-                    <template v-slot:end>
-                        <Button label="Exportar" icon="pi pi-upload" severity="help" @click="exportCSV($event)" />
-                    </template>
                 </Toolbar>
 
                 <DataTable
-                    ref="dt"
-                    :value="carreras"
-                    dataKey="id"
-                    :paginator="true"
-                    :rows="10"
+                    :value="cursosTable"
                     :filters="filters"
+                    v-model:expandedRows="expandedRows"
+                    dataKey="curso_id"
+                    tableStyle="min-width: 60rem"
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     :rowsPerPageOptions="[5, 10, 25]"
-                    currentPageReportTemplate="Mostrando del {first} al {last} de {totalRecords} carreras"
+                    currentPageReportTemplate="Mostrando del {first} al {last} de {totalRecords} cursos"
                 >
                     <template #header>
                         <div class="flex justify-content-between flex-column sm:flex-row">
@@ -173,131 +147,94 @@ const clearFilter = () => {
                             </IconField>
                         </div>
                     </template>
-                    <template #empty> No hay registros. </template>
-                    <template #loading> Cargando... por favor espera </template>
-                    <template #paginatorstart>
-                        <Button icon="pi pi-refresh" @click="loadCarreras" />
-                    </template>
-                    <Column field="carrera_id" header="ID Carrera" :sortable="true"></Column>
-                    <Column field="nombre_carrera" header="Nombre de la Carrera" :sortable="true"></Column>
-                    <Column headerStyle="min-width:10rem;" header="Acciones">
-                        <template #body="{ data }">
-                            <Button icon="pi pi-pencil" class="mr-2" severity="success" rounded @click="editCarrera(data)" />
-                            <Button icon="pi pi-trash" class="mt-2" severity="warning" rounded @click="confirmDeleteMateria(data)" />
+                    <Column :expander="true" headerStyle="width: 3rem" />
+                    <Column field="curso_id" header="ID Curso" :sortable="true">
+                        <template #body="slotProps">
+                            {{ slotProps.data.curso_id }}
                         </template>
                     </Column>
+                    <Column field="nombre_curso" header="Curso" :sortable="true">
+                        <template #body="slotProps">
+                            {{ slotProps.data.nombre_curso }}
+                        </template>
+                    </Column>
+                    <Column field="nombre_carrera" header="Carrera" :sortable="true">
+                        <template #body="slotProps">
+                            {{ slotProps.data.carrera.nombre_carrera }}
+                        </template>
+                    </Column>
+                    <Column headerStyle="min-width:10rem;" header="Acciones">
+                        <template #body="{ data }">
+                            <Button icon="pi pi-pencil" class="mr-2" severity="success" rounded @click="editCursos(data)" />
+                        </template>
+                    </Column>
+
+                    <template #expansion="slotProps">
+                        <div class="card">
+                            <h5>
+                                Detalle del Curso - <strong>{{ slotProps.data.nombre_curso }}</strong>
+                            </h5>
+                            <DataTable :value="slotProps.data.det_cursos">
+                                <Column field="det_curso_id" header="ID Detalle" :sortable="true">
+                                    <template #body="slotProps">
+                                        {{ slotProps.data.det_curso_id }}
+                                    </template>
+                                </Column>
+                                <Column field="nombre_materia" header="Nombre de la Materia" :sortable="true">
+                                    <template #body="slotProps">
+                                        {{ slotProps.data.materia.nombre_materia }}
+                                    </template>
+                                </Column>
+                                <Column headerStyle="width:4rem">
+                                    <template #body>
+                                        <Button icon="pi pi-search" @click="$router.push('materias')" />
+                                    </template>
+                                </Column>
+                            </DataTable>
+                        </div>
+                    </template>
                 </DataTable>
 
-                <Dialog v-model:visible="carreraModal" :header="isEditMode ? 'Datos de la Carrera - Editar' : 'Datos de la Carrera - Registrar'" :modal="true" class="p-fluid">
+                <Dialog v-model:visible="cursosModal" :style="{ width: '650px' }" :header="isEditMode ? 'Datos de la Carrera - Editar' : 'Datos de la Carrera - Registrar'" :modal="true" class="p-fluid">
                     <div class="field" v-if="isEditMode">
-                        <label for="carrera_id">ID Carrera</label>
-                        <InputText id="carrera_id" :disabled="isEditMode" v-model.trim="carreraEdit.carrera_id" />
+                        <label for="curso_id">ID Carrera</label>
+                        <InputText id="curso_id" :disabled="isEditMode" v-model.trim="cursosEdit.curso_id" />
                     </div>
                     <div class="field">
-                        <label for="nombre_carrera">Nombre de la Carrera</label>
-                        <InputText id="nombre_carrera" v-model.trim="carreraEdit.nombre_carrera" required="true" :invalid="submitted && !carreraEdit.nombre_carrera" />
-                        <small class="p-invalid" v-if="submitted && !carreraEdit.nombre_carrera">El numero de nombre carrera es requerida.</small>
+                        <label for="nombre_carrera">Carrera</label>
+                        <Dropdown id="nombre_carrera" v-model.trim="cursosEdit.carrera" :options="carreras" optionLabel="nombre_carrera" placeholder="Selecciona" required="true" :invalid="submitted && !cursosEdit.carrera" />
+                        <small class="p-invalid" v-if="submitted && !cursosEdit.carrera">La carrera es requerida.</small>
+                    </div>
+
+                    <div class="field">
+                        <label for="nombre_curso">Nombre del Curso</label>
+                        <InputText id="nombre_curso" v-model.trim="cursosEdit.nombre_curso" required="true" :invalid="submitted && !cursosEdit.nombre_curso" />
+                        <small class="p-invalid" v-if="submitted && !cursosEdit.nombre_curso">El numero de nombre carrera es requerida.</small>
+                    </div>
+                    <div class="field" v-if="!isEditMode">
+                        <label for="materia">Selecciona las materias</label>
+                        <MultiSelect v-model="cursosEdit.materias" :options="materias" :selectionLimit="5" optionLabel="nombre_materia" placeholder="Selecciona la materia" :filter="true">
+                            <template #value="slotProps">
+                                <div class="inline-flex align-items-center py-1 px-2 bg-blue-900 text-white border-round mr-2" v-for="option of slotProps.value" :key="option.id">
+                                    <div>{{ option.nombre_materia }}</div>
+                                </div>
+                                <template v-if="!slotProps.value || slotProps.value.length === 0">
+                                    <div class="p-1">Selecciona la materia</div>
+                                </template>
+                            </template>
+                            <template #option="slotProps">
+                                <div>{{ slotProps.option.nombre_materia }}</div>
+                            </template>
+                        </MultiSelect>
                     </div>
 
                     <!-- </ScrollPanel> -->
                     <template #footer>
                         <Button label="Cancelar" icon="pi pi-times" text="" @click="hideDialog" />
-                        <Button label="Guardar" icon="pi pi-check" text="" @click="saveCarrera" />
-                    </template>
-                </Dialog>
-
-                <Dialog v-model:visible="deleteCarreraModal" :style="{ width: '450px' }" header="Confirm" :modal="true">
-                    <div class="flex align-items-center justify-content-center">
-                        <i class="pi pi-exclamation-triangle mr-3" style="font-size: 2rem" />
-                        <span v-if="carreraEdit"
-                            >¿Estás seguro que quieres eliminar a ID Carrera: <b>{{ carreraEdit.carrera_id }}</b
-                            >?</span
-                        >
-                    </div>
-                    <template #footer>
-                        <Button label="No" icon="pi pi-times" text @click="deleteCarreraModal = false" />
-                        <Button label="Yes" icon="pi pi-check" text @click="deleteCarrera" />
+                        <Button label="Guardar" icon="pi pi-check" text="" @click="saveCurso" />
                     </template>
                 </Dialog>
             </div>
         </div>
     </div>
-
-    <!-- <DataTable :value="products" v-model:expandedRows="expandedRows" dataKey="id" tableStyle="min-width: 60rem">
-        <template #header>
-            <div>
-                <Button icon="pi pi-plus" label="Expand All" @click="expandAll" class="mr-2 mb-2" />
-                <Button icon="pi pi-minus" label="Collapse All" @click="collapseAll" class="mb-2" />
-            </div>
-        </template>
-        <Column :expander="true" headerStyle="width: 3rem" />
-        <Column field="name" header="Name" :sortable="true">
-            <template #body="slotProps">
-                {{ slotProps.data.name }}
-            </template>
-        </Column>
-        <Column header="Image">
-            <template #body="slotProps">
-                <img :src="'/demo/images/product/' + slotProps.data.image" :alt="slotProps.data.image" class="shadow-2" width="100" />
-            </template>
-        </Column>
-        <Column field="price" header="Price" :sortable="true">
-            <template #body="slotProps">
-                {{ formatCurrency(slotProps.data.price) }}
-            </template>
-        </Column>
-        <Column field="category" header="Category" :sortable="true">
-            <template #body="slotProps">
-                {{ formatCurrency(slotProps.data.category) }}
-            </template></Column
-        >
-        <Column field="rating" header="Reviews" :sortable="true">
-            <template #body="slotProps">
-                <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" />
-            </template>
-        </Column>
-        <Column field="inventoryStatus" header="Status" :sortable="true">
-            <template #body="slotProps">
-                <Tag :severity="getBadgeSeverity(slotProps.data.inventoryStatus)">{{ slotProps.data.inventoryStatus }}</Tag>
-            </template>
-        </Column>
-        <template #expansion="slotProps">
-            <div class="p-3">
-                <h5>Orders for {{ slotProps.data.name }}</h5>
-                <DataTable :value="slotProps.data.orders">
-                    <Column field="id" header="Id" :sortable="true">
-                        <template #body="slotProps">
-                            {{ slotProps.data.id }}
-                        </template>
-                    </Column>
-                    <Column field="customer" header="Customer" :sortable="true">
-                        <template #body="slotProps">
-                            {{ slotProps.data.customer }}
-                        </template>
-                    </Column>
-                    <Column field="date" header="Date" :sortable="true">
-                        <template #body="slotProps">
-                            {{ slotProps.data.date }}
-                        </template>
-                    </Column>
-                    <Column field="amount" header="Amount" :sortable="true">
-                        <template #body="slotProps">
-                            {{ formatCurrency(slotProps.data.amount) }}
-                        </template>
-                    </Column>
-                    <Column field="status" header="Status" :sortable="true">
-                        <template #body="slotProps">
-                            <span :class="'order-badge order-' + (slotProps.data.status ? slotProps.data.status.toLowerCase() : '')">{{ slotProps.data.status }}</span>
-                        </template>
-                    </Column>
-                    <Column headerStyle="width:4rem">
-                        <template #body>
-                            <Button icon="pi pi-search" />
-                        </template>
-                    </Column>
-                </DataTable>
-            </div>
-        </template>
-    </DataTable>
- -->
 </template>
