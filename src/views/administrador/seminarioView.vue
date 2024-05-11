524ee85db4fa1514d5165bfe87ac1181485ed4fe
HugoRomz <rosalesrafael1@hotmail.com>
2024-05-11 12:31:03 -0600

    Vista mejorada de los seminarios

--- src/views/administrador/seminarioView.vue
+++ src/views/administrador/seminarioView.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, inject } from 'vue';
 import { FilterMatchMode } from 'primevue/api';
+const toast = inject('toast');
 
 import SeminarioApi from '../../api/SeminarioApi.js';
 
@@ -10,6 +11,8 @@ const filters = ref();
 const loading = ref(false);
 const cursosData = ref(null);
 const rechazarCurso = ref(false);
+const verMotivo = ref(false);
+const altaCursoModal = ref(false);
 const dataCursoModal = ref({});
 
 const loadSeminarios = async () => {
@@ -17,7 +20,6 @@ const loadSeminarios = async () => {
     try {
         const response = await SeminarioApi.findSeminarioActivo();
         cursosData.value = response.data;
-        console.log('cursosData:', cursosData.value);
     } catch (error) {
         console.error('Error al obtener los usuarios:', error);
     } finally {
@@ -33,8 +35,29 @@ const rechazarCursoModal = (dataCurso) => {
 };
 
 const rechazarCursoMotivo = async (dataCurso) => {
-    console.log('dataCurso:', dataCurso);
-    rechazarCurso.value = false;
+    loading.value = true;
+    try {
+        const response = await SeminarioApi.rechazarCurso(dataCurso);
+        toast.open({
+            message: response.data.msg,
+            type: 'success'
+        });
+        loadSeminarios();
+    } catch (error) {
+        console.error('Error al rechazar el curso:', error);
+    } finally {
+        loading.value = false;
+        rechazarCurso.value = false;
+        dataCursoModal.value = {};
+    }
+};
+
+const verMotivoModal = (data) => {
+    verMotivo.value = true;
+    dataCursoModal.value = data;
+};
+const openModalCurso = () => {
+    altaCursoModal.value = true;
 };
 
 const initFilters = () => {
@@ -55,12 +78,13 @@ const clearFilter = () => {
             <div class="card">
                 <Toolbar class="mb-4">
                     <template v-slot:start>
-                        <div class="my-2">
-                            <!-- <Button label="Nueva Materia" icon="pi pi-plus" class="mr-2" severity="success" @click="openNew" /> -->
-                        </div>
+                        <div class="my-2"><Button label="Periodo" icon="pi pi-calendar-times" iconPos="right" class="mr-2" severity="primary" @click="console.log('Periodo')" /></div>
+                        <div class="my-2"><Button label="Alta Curso" icon="pi pi-plus" class="mr-2" severity="success" @click="openModalCurso" /></div>
                     </template>
+                    <template v-slot:center> </template>
 
                     <template v-slot:end>
+                        <div class="my-2"><Button label="Crear Catalogo Curso" icon="pi pi-plus" class="mr-2" severity="warning" @click="$router.push('createSeminarios')" /></div>
                         <Button label="Exportar" icon="pi pi-upload" severity="help" @click="exportCSV($event)" />
                     </template>
                 </Toolbar>
@@ -72,6 +96,8 @@ const clearFilter = () => {
                     :paginator="true"
                     :rows="10"
                     :filters="filters"
+                    :sortField="'periodo.descripcion'"
+                    :sortOrder="-1"
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     :rowsPerPageOptions="[5, 10, 25]"
                     currentPageReportTemplate="Mostrando del {first} al {last} de {totalRecords} cursos"
@@ -96,10 +122,10 @@ const clearFilter = () => {
                     <Column field="curso.carrera.nombre_carrera" header="Carrera" :sortable="true"></Column>
                     <Column field="status" header="Status" :sortable="true">
                         <template #body="{ data }">
-                            <Tag v-if="data.status == 'Pendiente'" class="mr-2" severity="warning" value="Pendiente"></Tag>
+                            <Tag v-if="data.status == 'Pendiente'" class="mr-2" severity="info" value="Pendiente"></Tag>
                             <Tag v-if="data.status == 'Aceptado'" class="mr-2" severity="success" value="Activo"></Tag>
-                            <Tag v-if="data.status == 'Cancelado'" class="mr-2" severity="info" value="Cancelado"></Tag>
-                            <Tag v-if="data.status == 'Finalizado'" class="mr-2" severity="danger" value="Terminado"></Tag>
+                            <Tag v-if="data.status == 'Cancelado'" class="mr-2" severity="warning" value="Cancelado"></Tag>
+                            <Tag v-if="data.status == 'Finalizado'" class="mr-2" severity="danger" value="Finalizado"></Tag>
                         </template>
                     </Column>
                     <Column headerStyle="min-width:10rem;" header="Acciones">
@@ -108,31 +134,13 @@ const clearFilter = () => {
                                 <Button v-if="data.status == 'Pendiente'" label="Aceptar" class="mr-2" rounded severity="success" @click="console.log('Aceptar')" />
                                 <Button v-if="data.status == 'Pendiente'" label="Rechazar" class="mr-2" rounded severity="danger" @click="rechazarCursoModal(data)" />
                                 <Button v-if="data.status == 'Aceptado'" label="Ver detalle" class="mr-2" rounded severity="info" @click="console.log('Ver detalle Seminario')" />
-                                <Button v-if="data.status == 'Cancelado'" label="Ver motivo" class="mr-2" rounded severity="warning" @click="console.log('Ver motivo')" />
+                                <Button v-if="data.status == 'Cancelado'" label="Ver motivo" class="mr-2" rounded severity="warning" @click="verMotivoModal(data)" />
                                 <Button v-if="data.status == 'Finalizado'" label="Ver reporte" class="mr-2" rounded severity="help" @click="console.log('Ver reporte Seminario')" />
                             </div>
                         </template>
                     </Column>
                 </DataTable>
 
-                <!-- <Dialog v-model:visible="materiaModal" :header="isEditMode ? 'Datos de la Materia - Editar' : 'Datos de la Materia - Registrar'" :modal="true" class="p-fluid">
-                    <div class="field" v-if="isEditMode">
-                        <label for="materia_id">ID Materia</label>
-                        <InputText id="materia_id" :disabled="isEditMode" v-model.trim="materiaEdit.materia_id" />
-                    </div>
-                    <div class="field">
-                        <label for="nombre_materia">Nombre de la Materia</label>
-                        <InputText id="nombre_materia" v-model.trim="materiaEdit.nombre_materia" required="true" :invalid="submitted && !materiaEdit.nombre_materia" />
-                        <small class="p-invalid" v-if="submitted && !materiaEdit.nombre_materia">El numero de nombre materia es requerida.</small>
-                    </div>
-
-                  
-                    <template #footer>
-                        <Button label="Cancelar" icon="pi pi-times" text="" @click="hideDialog" />
-                        <Button label="Guardar" icon="pi pi-check" text="" @click="saveMateria" />
-                    </template>
-                </Dialog>
- -->
                 <Dialog v-model:visible="rechazarCurso" :style="{ width: '450px' }" header="Rechazar Curso" :modal="true">
                     <div class="flex flex-column align-items-center justify-content-center">
                         <i class="pi pi-exclamation-triangle" style="font-size: 2rem" />
@@ -140,8 +148,35 @@ const clearFilter = () => {
                         <InputText class="mt-3 w-8" v-model="dataCursoModal.motivo" placeholder="Motivo de rechazo" />
                     </div>
                     <template #footer>
-                        <Button label="No" icon="pi pi-times" text @click="rechazarCurso = false" />
+                        <Button label="No" icon="pi pi-times" text @click="(rechazarCurso = false), (dataCursoModal.motivo = '')" />
                         <Button label="Yes" icon="pi pi-check" text @click="rechazarCursoMotivo(dataCursoModal)" />
+                    </template>
+                </Dialog>
+
+                <Dialog v-model:visible="verMotivo" :style="{ width: '450px' }" header="Motivo de Rechazo" :modal="true">
+                    <div class="flex flex-column align-items-center justify-content-center">
+                        <i class="pi pi-info-circle mt-1" style="font-size: 2rem" />
+                        <span class="mt-3 text-lg" v-if="dataCursoModal"
+                            >Descripción: <strong> {{ dataCursoModal.descripcion }}</strong></span
+                        >
+                    </div>
+                    <template #footer>
+                        <Button label="Cerrar" icon="pi pi-times" text @click="verMotivo = false" />
+                    </template>
+                </Dialog>
+
+                <Dialog v-model:visible="altaCursoModal" :style="{ width: '450px' }" header="Alta Curso" :modal="true">
+                    <div class="field">
+                        <label for="materia_id">ID Materia</label>
+                        <InputText id="materia_id" />
+                    </div>
+                    <div class="field">
+                        <label for="nombre_materia">Nombre de la Materia</label>
+                        <InputText id="nombre_materia" required="true" />
+                    </div>
+                    <template #footer>
+                        <!-- <Button label="Cancelar" icon="pi pi-times" text="" @click="hideDialog" />
+                        <Button label="Guardar" icon="pi pi-check" text="" @click="saveMateria" /> -->
                     </template>
                 </Dialog>
             </div>
