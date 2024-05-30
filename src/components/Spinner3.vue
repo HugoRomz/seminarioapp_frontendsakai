6ec52b9766f722914c4d69c2b8f2b361e1f3c589
Hugo Rafael Rosales Melendez <85382986+HugoRomz@users.noreply.github.com>
2024-05-30 17:57:46 -0600

    Update Spinner3.vue

--- src/components/Spinner3.vue
+++ src/components/Spinner3.vue
@@ -6,63 +6,27 @@ const loading = ref(false);
 const router = useRouter();
 
 router.beforeEach(() => {
-    loading.value = true;
+  loading.value = true;
 });
 
 router.afterEach(() => {
-    loading.value = false;
+  loading.value = false;
 });
 </script>
 
 <template>
-    <div>
-        <div class="spinner-overlay" v-if="loading">
-            <span class="loader"></span>
-        </div>
-        <router-view />
-    </div>
+  <div class="loading-overlay" v-if="loading"></div>
+  <router-view />
 </template>
 
 <style scoped>
-.spinner-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(255, 255, 255, 0.7);
-    z-index: 9999;
-}
-
-.loader {
-    width: 64px;
-    height: 64px;
-    position: relative;
-    background-image: linear-gradient(#d4b012 16px, transparent 0), linear-gradient(#00294f 16px, transparent 0), linear-gradient(#00294f 16px, transparent 0), linear-gradient(#d4b012 16px, transparent 0);
-    background-repeat: no-repeat;
-    background-size: 16px 16px;
-    background-position: left top, left bottom, right top, right bottom;
-    animation: rotate 1s linear infinite;
-}
-
-@keyframes rotate {
-    0% {
-        width: 64px;
-        height: 64px;
-        transform: rotate(0deg);
-    }
-    50% {
-        width: 30px;
-        height: 30px;
-        transform: rotate(180deg);
-    }
-    100% {
-        width: 64px;
-        height: 64px;
-        transform: rotate(360deg);
-    }
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Fondo negro transparente */
+  z-index: 9999;
 }
 </style>
